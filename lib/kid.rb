cc45f18d2c99ae0e4a583ebb065edf98ca220f6d
Iuliia El Sammak <juliag875@gmail.com>
2022-02-27 15:53:23 -0500

    commit tree 0bdd06341ecf0dab493c25330bd0101be9fc7f00

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,25 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance.rb'
+
+class Kid 
+  include Dance
+  extend MetaDancing
+  
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+# with non-namespaced modules
+# class Kid
+#   include Dance
+#   extend MetaDancing
+#   attr_accessor :name
+
+#   def initialize(name)
+#     @name = name
+#   end
+# end
